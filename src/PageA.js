550b8d2bacb1cf26f0be5404572f72c4ff0edeae
M.Yoneyama <mnbynym@gmail.com>
2020-06-24 14:10:58 +0900

    commit

--- src/PageA.js
+++ src/PageA.js
@@ -3,11 +3,19 @@ import { Link } from 'react-router-dom';
 
 class PageA extends Component {
 
+    handleButtonClick = () => {
+        this.props.history.push('/b')
+    }
+
     render() {
         return (
             <div>
                 <h1>Page-A</h1>
                 <Link to="b">Page-B</Link>
+                <br />
+                <button onClick={this.handleButtonClick}>
+                    Page-B
+                </button>
             </div>
         );
     }
